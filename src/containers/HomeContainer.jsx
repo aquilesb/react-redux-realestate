6ee0e22f87d78f7ce8c6832a87d1f51089af7250
aquilesds <aquilesds@clinicloud.com>
2019-02-15 11:30:12 +1100

    Changing container from class to pure functions

--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
@@ -14,132 +14,129 @@ import PropertyListItem from '../components/PropertyListItem';
 import PropertyRecommended from '../components/PropertyRecommended';
 import { carouselDots } from '../utils/carouselUtils';
 
-class HomeContainer extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.closeLoginModal = this.closeLoginModal.bind(this);
-    this.openLoginModal = this.openLoginModal.bind(this);
-  }
+const HomeContainer = ({
+  featuredProperties,
+  loginModalOpen,
+  recommendedProperties,
+  priceTypes,
+  searchType,
+  loginAction,
+  updateLoginModalAction,
+  onSearchPropertyAction,
+  changeSearchFieldAction,
+}) => {
+  const [isRactModalAttached, setReactModal] = useState(false);
 
-  componentWillMount() {
-    ReactModal.setAppElement('body');
-  }
+  useEffect(() => {
+    if (!isRactModalAttached) {
+      setReactModal(true);
+      ReactModal.setAppElement('body');
+    }
+  });
 
-  closeLoginModal() {
-    this.props.updateLoginModalIsOpen(false);
-  }
+  const closeLoginModal = () => updateLoginModalAction(false);
 
-  openLoginModal(e) {
+  const openLoginModal = (e) => {
     e.preventDefault();
-    this.props.updateLoginModalIsOpen(true);
-  }
+    updateLoginModalAction(true);
+  };
 
-  render() {
-    const {
-      featuredProperties,
-      loginModalOpen,
-      recommendedProperties,
-      priceTypes,
-      searchType,
-      loginAction,
-    } = this.props;
-    const priceListItem = priceTypes.find(item => item.get('type') === searchType);
+  const priceListItem = priceTypes.find(item => item.get('type') === searchType);
 
-    return (
-      <main className="home">
-        <div className="banner-search">
-          <div className="search-wrapper">
-            <div className="container">
-              <h3>Buy, Sale & Rent</h3>
-              <form className="searchbar" onSubmit={this.props.onSearchProperty}>
-                <div className="row">
-                  <div className="col-lg-6 col-sm-6">
-                    <input type="text" className="form-control" placeholder="Search of Properties" />
-                    <div className="row">
-                      <div className="col-lg-3 col-sm-3 ">
-                        <select className="form-control buy" value={searchType} onChange={e => this.props.changeSearchField('type', e.target.value)}>
-                          <option value="1">Buy</option>
-                          <option value="2">Rent</option>
-                          <option value="3">Sale</option>
-                        </select>
-                      </div>
-                      <div className="col-lg-3 col-sm-4">
-                        <select className="form-control price" onChange={e => this.props.changeSearchField('price', e.target.value)}>
-                          <option value="">Price</option>
-                          {!!priceListItem.get('list') && priceListItem.get('list').map(item => <option key={item}>{item}</option>)}
-                        </select>
-                      </div>
-                      <div className="col-lg-3 col-sm-4">
-                        <select className="form-control property" onChange={e => this.props.changeSearchField('property', e.target.value)}>
-                          <option value="">Property</option>
-                          <option value="1">Apartment</option>
-                          <option value="2">Building</option>
-                          <option value="3">Office Space</option>
-                        </select>
-                      </div>
-                      <div className="col-lg-3 col-sm-4">
-                        <input type="submit" className="btn btn-success btn-find-now" value="Find Now" />
-                      </div>
+  return (
+    <main className="home">
+      <div className="banner-search">
+        <div className="search-wrapper">
+          <div className="container">
+            <h3>Buy, Sale &amp; Rent</h3>
+            <form className="searchbar" onSubmit={onSearchPropertyAction}>
+              <div className="row">
+                <div className="col-lg-6 col-sm-6">
+                  <input type="text" className="form-control" placeholder="Search of Properties" />
+                  <div className="row">
+                    <div className="col-lg-3 col-sm-3 ">
+                      <select className="form-control buy" value={searchType} onChange={e => changeSearchFieldAction('type', e.target.value)}>
+                        <option value="1">Buy</option>
+                        <option value="2">Rent</option>
+                        <option value="3">Sale</option>
+                      </select>
+                    </div>
+                    <div className="col-lg-3 col-sm-4">
+                      <select className="form-control price" onChange={e => changeSearchFieldAction('price', e.target.value)}>
+                        <option value="">Price</option>
+                        {!!priceListItem.get('list') && priceListItem.get('list').map(item => <option key={item}>{item}</option>)}
+                      </select>
+                    </div>
+                    <div className="col-lg-3 col-sm-4">
+                      <select className="form-control property" onChange={e => changeSearchFieldAction('property', e.target.value)}>
+                        <option value="">Property</option>
+                        <option value="1">Apartment</option>
+                        <option value="2">Building</option>
+                        <option value="3">Office Space</option>
+                      </select>
+                    </div>
+                    <div className="col-lg-3 col-sm-4">
+                      <input type="submit" className="btn btn-success btn-find-now" value="Find Now" />
                     </div>
                   </div>
-                  <div className="col-lg-5 col-lg-offset-1 col-sm-6 ">
-                    <p>Join now and get updated with all the properties deals.</p>
-                    <button className="btn btn-info" onClick={this.openLoginModal}>Login</button>
-                  </div>
                 </div>
-              </form>
+                <div className="col-lg-5 col-lg-offset-1 col-sm-6 ">
+                  <p>Join now and get updated with all the properties deals.</p>
+                  <button className="btn btn-info" onClick={openLoginModal}>Login</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <section className="content-container container">
+        <div className="properties-listing spacer">
+          <Link to="/search" className="pull-right viewall">View All Listing</Link>
+          <h2>Featured Properties</h2>
+          <Carousel
+            id="featured-properties-list"
+            slidesToShow={5}
+            cellSpacing={10}
+            swiping
+            renderBottomCenterControls={carouselDots}
+          >
+            {featuredProperties.map(item => <PropertyListItem key={item.get('id')} item={item} />)}
+          </Carousel>
+        </div>
+        <div className="spacer">
+          <div className="row">
+            <div className="col-lg-6 col-sm-9 recent-view">
+              <h3>About Us</h3>
+              <p>
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for
+                 those interested. Sections 1.10.32 and 1.10.33 from &#x22;de Finibus Bonorum
+                et Malorum&#x22; by Cicero are also reproduced in their exact original form,
+                 accompanied by English versions from the 1914 translation by H. Rackham.
+                <br />
+                <Link to="/about">Learn More</Link>
+              </p>
+            </div>
+            <div className="col-lg-5 col-lg-offset-1 col-sm-3 recommended">
+              <h3>Recommended Properties</h3>
+              <Carousel
+                id="recommended-properties-list"
+                slidesToShow={1}
+                swiping
+                renderBottomCenterControls={carouselDots}
+                decorators={[]}
+              >
+                {recommendedProperties.map(item => <PropertyRecommended item={item} key={item.get('id')} />)}
+              </Carousel>
             </div>
           </div>
         </div>
-        <section className="content-container container">
-          <div className="properties-listing spacer">
-            <Link to="/search" className="pull-right viewall">View All Listing</Link>
-            <h2>Featured Properties</h2>
-            <Carousel
-              id="featured-properties-list"
-              slidesToShow={5}
-              cellSpacing={10}
-              swiping
-              renderBottomCenterControls={carouselDots}
-            >
-              { featuredProperties.map(item => <PropertyListItem key={item.get('id')} item={item} />) }
-            </Carousel>
-          </div>
-          <div className="spacer">
-            <div className="row">
-              <div className="col-lg-6 col-sm-9 recent-view">
-                <h3>About Us</h3>
-                <p>
-                  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for
-                   those interested. Sections 1.10.32 and 1.10.33 from &#x22;de Finibus Bonorum
-                  et Malorum&#x22; by Cicero are also reproduced in their exact original form,
-                   accompanied by English versions from the 1914 translation by H. Rackham.
-                  <br />
-                  <Link to="/about">Learn More</Link>
-                </p>
-              </div>
-              <div className="col-lg-5 col-lg-offset-1 col-sm-3 recommended">
-                <h3>Recommended Properties</h3>
-                <Carousel
-                  id="recommended-properties-list"
-                  slidesToShow={1}
-                  swiping
-                  renderBottomCenterControls={carouselDots}
-                  decorators={[]}
-                >
-                  {recommendedProperties.map(item => <PropertyRecommended item={item} key={item.get('id')} />)}
-                </Carousel>
-              </div>
-            </div>
-          </div>
-        </section>
-        <ReactModal isOpen={loginModalOpen} onRequestClose={this.closeLoginModal} contentLabel="Modal" overlayClassName="modal-overlay" className="modal-content">
-          <LoginModal login={loginAction} />
-        </ReactModal>
-      </main>
-    );
-  }
-}
+      </section>
+      <ReactModal isOpen={loginModalOpen} onRequestClose={closeLoginModal} contentLabel="Modal" overlayClassName="modal-overlay" className="modal-content">
+        <LoginModal login={loginAction} />
+      </ReactModal>
+    </main>
+  );
+};
 
 HomeContainer.propTypes = {
   featuredProperties: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
@@ -168,9 +165,9 @@ HomeContainer.propTypes = {
   }).isRequired,
   searchType: PropTypes.string.isRequired,
   loginModalOpen: PropTypes.bool.isRequired,
-  updateLoginModalIsOpen: PropTypes.func.isRequired,
-  changeSearchField: PropTypes.func.isRequired,
-  onSearchProperty: PropTypes.func.isRequired,
+  updateLoginModalAction: PropTypes.func.isRequired,
+  changeSearchFieldAction: PropTypes.func.isRequired,
+  onSearchPropertyAction: PropTypes.func.isRequired,
   loginAction: PropTypes.func.isRequired,
 };
 
@@ -183,9 +180,9 @@ const mapStateToProps = state => ({
 });
 
 const functions = {
-  updateLoginModalIsOpen,
-  changeSearchField,
-  onSearchProperty,
+  updateLoginModalAction: updateLoginModalIsOpen,
+  changeSearchFieldAction: changeSearchField,
+  onSearchPropertyAction: onSearchProperty,
   loginAction: login,
 };
 
--- src/containers/PropertyContainer.jsx
+++ src/containers/PropertyContainer.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
@@ -9,91 +9,88 @@ import PropertyHot from '../components/PropertyHot';
 import { format2Money } from '../utils/formatUtils';
 import { carouselDots } from '../utils/carouselUtils';
 
-class PropertyContainer extends PureComponent {
-  render() {
-    const { property, hotProps } = this.props;
-    if (!property) {
-      return null;
-    }
+const PropertyContainer = ({ property, hotProps }) => {
+  if (!property) {
+    return null;
+  }
 
-    return (
-      <main className="property-section">
-        <InsideBanner title="Property Detail" />
-        <section className="container">
-          <div className="properties-listing spacer">
-            <div className="row">
-              <div className="col-lg-3 col-sm-4 hidden-xs">
-                <div className="hot-properties hidden-xs">
-                  <h4>Hot Properties</h4>
-                  { !!hotProps && hotProps.size > 0 && hotProps.map(item => <PropertyHot item={item} key={item.get('id')} />)}
-                </div>
-                <div className="advertisement">
-                  <h4>Advertisements</h4>
-                  <img src="/static/images/advertisements.jpg" className="img-responsive" alt="advertisement" />
-                </div>
+  return (
+    <main className="property-section">
+      <InsideBanner title="Property Detail" />
+      <section className="container">
+        <div className="properties-listing spacer">
+          <div className="row">
+            <div className="col-lg-3 col-sm-4 hidden-xs">
+              <div className="hot-properties hidden-xs">
+                <h4>Hot Properties</h4>
+                {!!hotProps && hotProps.size > 0 && hotProps.map(item => <PropertyHot item={item} key={item.get('id')} />)}
               </div>
-              <div className="col-lg-9 col-sm-8 name">
-                <h2>{property.get('name')}</h2>
-                <div className="row">
-                  <div className="col-lg-8">
-                    <div className="property-images">
-                      <Carousel
-                        slidesToShow={1}
-                        swiping
-                        renderBottomCenterControls={carouselDots}
-                      >
-                        {property.get('images').map(url => <img src={url} key={url} className="property-img" alt="properties" />)}
-                      </Carousel>
-                    </div>
-                    <div className="spacer detail">
-                      <h4>
-                        <span className="glyphicon glyphicon-th-list" />
-                        Properties Detail
-                      </h4>
-                      {renderHTML(property.get('detail'))}
-                    </div>
-                    <div>
-                      <h4>
-                        <span className="glyphicon glyphicon-map-marker" />
-                        Location
-                      </h4>
-                      <div className="well">
-                        <iframe width="100%" height="350" title="map" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Pulchowk,+Patan,+Central+Region,+Nepal&amp;aq=0&amp;oq=pulch&amp;sll=37.0625,-95.677068&amp;sspn=39.371738,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Pulchowk,+Patan+Dhoka,+Patan,+Bagmati,+Central+Region,+Nepal&amp;ll=27.678236,85.316853&amp;spn=0.001347,0.002642&amp;t=m&amp;z=14&amp;output=embed" />
-                      </div>
+              <div className="advertisement">
+                <h4>Advertisements</h4>
+                <img src="/static/images/advertisements.jpg" className="img-responsive" alt="advertisement" />
+              </div>
+            </div>
+            <div className="col-lg-9 col-sm-8 name">
+              <h2>{property.get('name')}</h2>
+              <div className="row">
+                <div className="col-lg-8">
+                  <div className="property-images">
+                    <Carousel
+                      slidesToShow={1}
+                      swiping
+                      renderBottomCenterControls={carouselDots}
+                    >
+                      {property.get('images').map(url => <img src={url} key={url} className="property-img" alt="properties" />)}
+                    </Carousel>
+                  </div>
+                  <div className="spacer detail">
+                    <h4>
+                      <span className="glyphicon glyphicon-th-list" />
+                      Properties Detail
+                    </h4>
+                    {renderHTML(property.get('detail'))}
+                  </div>
+                  <div>
+                    <h4>
+                      <span className="glyphicon glyphicon-map-marker" />
+                      Location
+                    </h4>
+                    <div className="well">
+                      <iframe width="100%" height="350" title="map" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Pulchowk,+Patan,+Central+Region,+Nepal&amp;aq=0&amp;oq=pulch&amp;sll=37.0625,-95.677068&amp;sspn=39.371738,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Pulchowk,+Patan+Dhoka,+Patan,+Bagmati,+Central+Region,+Nepal&amp;ll=27.678236,85.316853&amp;spn=0.001347,0.002642&amp;t=m&amp;z=14&amp;output=embed" />
                     </div>
                   </div>
-                  <div className="col-lg-4">
-                    <div className="col-lg-12  col-sm-6">
-                      <div className="property-info">
-                        <p className="price">$ {format2Money(property.get('price'))}</p>
-                        <p className="area">
-                          <span className="glyphicon glyphicon-map-marker" />
-                          {property.get('address')}
-                        </p>
-                        <div className="profile">
-                          <span className="glyphicon glyphicon-user" />
-                          Agent Details
-                          <p>{property.getIn(['agent', 'name'])}<br />{property.getIn(['agent', 'phone'])}</p>
-                        </div>
-                      </div>
-                      <h6><span className="glyphicon glyphicon-home" /> Availabilty</h6>
-                      <div className="listing-detail">
-                        <span title="Bed Room" className="bedroom">{property.getIn(['features', 'bedroom'])}</span>
-                        <span title="Baths" className="bathroom">{property.getIn(['features', 'bathroom'])}</span>
-                        <span title="Parking" className="parking">{property.getIn(['features', 'parking'])}</span>
-                        <span title="Kitchen" className="kitchen">{property.getIn(['features', 'kitchen'])}</span>
+                </div>
+                <div className="col-lg-4">
+                  <div className="col-lg-12  col-sm-6">
+                    <div className="property-info">
+                      <p className="price">$ {format2Money(property.get('price'))}</p>
+                      <p className="area">
+                        <span className="glyphicon glyphicon-map-marker" />
+                        {property.get('address')}
+                      </p>
+                      <div className="profile">
+                        <span className="glyphicon glyphicon-user" />
+                        Agent Details
+                        <p>{property.getIn(['agent', 'name'])}<br />{property.getIn(['agent', 'phone'])}</p>
                       </div>
                     </div>
-                    <div className="col-lg-12 col-sm-6 ">
-                      <div className="enquiry">
-                        <h6><span className="glyphicon glyphicon-envelope" />Post Enquiry</h6>
-                        <div role="form" className="contact-form">
-                          <input type="text" className="form-control name" placeholder="Full Name" />
-                          <input type="email" className="form-control email" placeholder="you@yourdomain.com" />
-                          <input type="text" className="form-control phone" placeholder="your number" />
-                          <textarea rows="6" className="form-control text" placeholder="Whats on your mind?" />
-                          <button type="submit" className="btn btn-primary" name="Submit">Send Message</button>
-                        </div>
+                    <h6><span className="glyphicon glyphicon-home" /> Availabilty</h6>
+                    <div className="listing-detail">
+                      <span title="Bed Room" className="bedroom">{property.getIn(['features', 'bedroom'])}</span>
+                      <span title="Baths" className="bathroom">{property.getIn(['features', 'bathroom'])}</span>
+                      <span title="Parking" className="parking">{property.getIn(['features', 'parking'])}</span>
+                      <span title="Kitchen" className="kitchen">{property.getIn(['features', 'kitchen'])}</span>
+                    </div>
+                  </div>
+                  <div className="col-lg-12 col-sm-6 ">
+                    <div className="enquiry">
+                      <h6><span className="glyphicon glyphicon-envelope" />Post Enquiry</h6>
+                      <div role="form" className="contact-form">
+                        <input type="text" className="form-control name" placeholder="Full Name" />
+                        <input type="email" className="form-control email" placeholder="you@yourdomain.com" />
+                        <input type="text" className="form-control phone" placeholder="your number" />
+                        <textarea rows="6" className="form-control text" placeholder="Whats on your mind?" />
+                        <button type="submit" className="btn btn-primary" name="Submit">Send Message</button>
                       </div>
                     </div>
                   </div>
@@ -101,11 +98,11 @@ class PropertyContainer extends PureComponent {
               </div>
             </div>
           </div>
-        </section>
-      </main>
-    );
-  }
-}
+        </div>
+      </section>
+    </main>
+  );
+};
 
 PropertyContainer.propTypes = {
   property: ImmutablePropTypes.contains({
