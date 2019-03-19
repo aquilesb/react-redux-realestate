5524bf4cc4421de0384ba683aaa5be4878d310ae
aquiles <aquilesb@hotmail.com>
2019-03-20 01:34:59 +1100

    Separating dev index and prod index

--- src/indexDev.jsx
+++ src/indexDev.jsx
@@ -4,10 +4,13 @@ import { Provider } from 'react-redux';
 import { fromJS } from 'immutable';
 import { createBrowserHistory } from 'history';
 import { ConnectedRouter } from 'connected-react-router/immutable';
+import { hot } from 'react-hot-loader/root'; // eslint-disable-line
+import { setConfig } from 'react-hot-loader'; // eslint-disable-line
 import configureStore from './store/configureStore';
 import IndexContainer from './containers/IndexContainer';
 import loadInitialData from './actions/startupActions';
-import './layout/index.scss';
+
+setConfig({ pureSFC: true });
 
 
 const history = createBrowserHistory();
@@ -30,4 +33,11 @@ const render = () => {
 
 render();
 
-store.dispatch(loadInitialData());
+require('./layout/index.scss'); // eslint-disable-line
+setTimeout(() => {
+  store.dispatch(loadInitialData());
+}, 500);
+
+if (module.hot) {
+  hot(App, { errorBoundary: false });
+}
