fbeee0f3bfb1b49e43ddc5106cf523940eaef130
aquilesds <aquilesds@clinicloud.com>
2019-03-28 12:48:34 +1100

    Adding flags to manage if agents were loaded or if it still loading

--- src/modules/Agents/Store/agents.reducer.js
+++ src/modules/Agents/Store/agents.reducer.js
@@ -2,13 +2,21 @@ import { fromJS } from 'immutable';
 import * as Actions from './agents.actionTypes';
 
 const initialState = fromJS({
+  isLoadingAgents: false,
+  isLoaded: false,
   list: [],
 });
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case Actions.AGENTS_GET_LIST_SUCCESS:
-      return state.set('list', action.payload);
+      return state.set('list', action.payload).set('isLoadingAgents', false).set('isLoaded', true);
+
+    case Actions.AGENTS_GET_LIST:
+      return state.set('isLoadingAgents', true);
+
+    case Actions.AGENTS_GET_LIST_FAILURE:
+      return state.set('isLoadingAgents', false).set('isLoaded', true);
 
     default:
       return state;
